82e3560350ccd19d18c990c795118b37cda93b4e
Steve Brudz <sbrudz@defmethod.io>
2020-09-25 17:57:57 -0500

    feat(deploy frequency): display histogram of deploys per day on Deploy Frequency chart

--- src/report.ts
+++ src/report.ts
@@ -4,7 +4,7 @@ import { getDeployments } from "./heroku-deployments";
 import { calculateDeploymentFrequency } from "./deployment-frequency";
 import { calculateAverageLeadTime } from "./lead-time";
 import ejs from "ejs";
-import { Duration } from "luxon";
+import { Duration, Interval } from "luxon";
 import fs from "fs/promises";
 
 export interface ReportParams extends RollingWindowInputs {
@@ -15,6 +15,15 @@ export interface ReportParams extends RollingWindowInputs {
 export async function generateReport(params: ReportParams) {
   const heroku = new Heroku({ token: process.env.HEROKU_API_TOKEN });
   const deployments = await getDeployments(params.herokuAppName, heroku);
+
+  const reportPeriod = Interval.fromDateTimes(
+    params.reportStart,
+    params.reportEnd
+  );
+  const deploymentTimestamps = deployments
+    .filter((d) => reportPeriod.contains(d.timeCreated))
+    .map((d) => d.timeCreated.toMillis());
+
   const windows = rollingWindows(params);
   const deployFreqData = calculateDeploymentFrequency(deployments, windows);
   const leadTimeData = await calculateAverageLeadTime(deployments, windows);
@@ -25,6 +34,7 @@ export async function generateReport(params: ReportParams) {
       windowSize: Duration.fromObject(params.sampleWindowSize).toFormat("d"),
       deployFreqData: JSON.stringify(deployFreqData),
       leadTimeData: JSON.stringify(leadTimeData),
+      deploymentTimestamps: JSON.stringify(deploymentTimestamps),
     },
     { async: true }
   );
