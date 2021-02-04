decda7e24cf5078d86816b6374e2531b42f509eb
Steve Brudz <sbrudz@defmethod.io>
2021-02-04 16:50:09 -0600

    feat: provide report generation as a github action

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,41 @@
+import * as core from "@actions/core";
 import { DateTime } from "luxon";
 import { generateReport } from "./report";
 
-const appName = process.env.HEROKU_APP_NAME;
-if (appName === undefined) {
-  throw new Error("HEROKU_APP_NAME environment variable must be defined.");
+async function run(): Promise<void> {
+  const appName = core.getInput("heroku_app_name");
+  if (appName === undefined) {
+    throw new Error("heroku_app_name must be defined.");
+  }
+  const reportFileName = "./report.html";
+
+  const reportEndInput = core.getInput("report_end_date");
+  const reportEnd = reportEndInput
+    ? DateTime.fromISO(reportEndInput)
+    : DateTime.fromJSDate(new Date()).endOf("day");
+
+  const reportTimeframe = core.getInput("report_timeframe");
+  const monthsBack = parseInt(reportTimeframe, 10);
+  const reportStart = reportEnd.minus({ months: monthsBack });
+
+  const reportParams = {
+    herokuAppName: appName,
+    reportFileName,
+    reportStart,
+    reportEnd,
+    samplingFrequency: { days: 3 },
+    sampleWindowSize: { days: 30 },
+  };
+
+  try {
+    await generateReport(reportParams);
+    core.debug(
+      `New report generated successfully. Available at ${reportFileName}`
+    );
+    core.setOutput("report", reportFileName);
+  } catch (error) {
+    core.setFailed(error);
+  }
 }
-const reportFileName = "./report.html";
 
-const reportEndEnvStr = process.env.REPORT_END_DATE;
-const reportEnd = reportEndEnvStr
-  ? DateTime.fromISO(reportEndEnvStr)
-  : DateTime.fromJSDate(new Date()).endOf("day");
-
-const reportStartEnvStr = process.env.REPORT_START_DATE;
-const reportStart = reportStartEnvStr
-  ? DateTime.fromISO(reportStartEnvStr)
-  : reportEnd.minus({ months: 3 });
-
-const reportParams = {
-  herokuAppName: appName,
-  reportFileName,
-  reportStart,
-  reportEnd,
-  samplingFrequency: { days: 3 },
-  sampleWindowSize: { days: 30 },
-};
-
-try {
-  generateReport(reportParams).then((params) => {
-    console.log(`New report generated. Available at ${params.reportFileName}`);
-  });
-} catch (e) {
-  console.error(e);
-}
+run();
