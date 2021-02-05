782eb7b8da9cc8b53fd8e0fa63923f8f7caf3670
Steve Brudz <sbrudz@defmethod.io>
2021-02-05 10:50:35 -0600

    refactor: retrieve git_project_directory from action inputs instead of environment variable

--- src/git-utils.ts
+++ src/git-utils.ts
@@ -1,3 +1,4 @@
+import * as core from "@actions/core";
 import { Deployment } from "./heroku-deployments";
 import parse from "csv-parse/lib/sync";
 import { DateTime } from "luxon";
@@ -6,13 +7,11 @@ import { exec as execCb } from "child_process";
 
 const exec = promisify(execCb);
 
-const gitProjectDirectory = process.env.GIT_PROJECT_DIRECTORY || ".";
-
 export const getCommitsBetweenRevisions = async (
   start: Deployment,
   end?: Deployment
 ): Promise<Deployment[]> => {
-  // TODO: Remove this hard coding of directory
+  const gitProjectDirectory = core.getInput("git_project_directory");
   const revisionQuery = end ? `${start.commit}..${end.commit}` : start.commit;
   const command = `cd ${gitProjectDirectory} && git log --pretty=format:"%h,%aI" "${revisionQuery}" --no-merges`;
   const { stdout, stderr } = await exec(command);
